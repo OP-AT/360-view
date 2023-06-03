4436327fd2033e0c464e9790bea72fde46c96f62
ashmit op tripathi <102431352+OP-AT@users.noreply.github.com>
2023-06-03 22:51:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
     container: imageContainer,
+    autoRotate: true,
+    autoRotateSpeed: 0.4,
+    controlBar: true,
 });
 
 viewer.add(panoramaImage);
